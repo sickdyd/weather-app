e242e76c38814a76d42c2f983b7017465205e2c7
Rob <roberto.reale.ja@gmail.com>
2021-03-21 18:00:35 +0900

    Add support for ie11

--- src/client.tsx
+++ src/client.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { hydrate } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
 
 import App from './client/App'
 
