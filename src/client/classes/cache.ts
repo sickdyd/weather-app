81bf29037ae50ee5ce208f7f57b00c6861ddd20d
Rob <roberto.reale.ja@gmail.com>
2021-03-21 12:52:30 +0900

    Refresh data every 5 minutes

--- src/client/classes/cache.ts
+++ src/client/classes/cache.ts
@@ -1,10 +1,22 @@
 class cache {
   storeData(cityName: string, weatherData: WeatherData): void {
-    sessionStorage.setItem(cityName, JSON.stringify(weatherData))
+    sessionStorage.setItem(
+      cityName,
+      JSON.stringify({ ...weatherData, expiresAt: this.expiration() })
+    )
   }
 
-  getData(cityName: string): WeatherData {
-    return JSON.parse(sessionStorage.getItem(cityName)) as WeatherData
+  getData(cityName: string): WeatherData | null {
+    const weatherData = JSON.parse(sessionStorage.getItem(cityName)) as WeatherData
+    return this.isExpired(weatherData?.expiresAt) ? null : weatherData
+  }
+
+  expiration(): number {
+    return new Date().getTime() + 300000
+  }
+
+  isExpired(expiresAt: number) {
+    return expiresAt > new Date().getTime() ? false : true
   }
 }
 
--- src/client/hooks/useWeatherData.ts
+++ src/client/hooks/useWeatherData.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import cache from '../classes/cache'
 import { requestDataByCity, requestDataByCoords } from '../requests/sendWeatherDataRequest'
 
 export const REFRESH_TIME_IN_MS = 300000
@@ -14,22 +15,42 @@ const useWeatherData: (
   const [queryCities] = useState(cities)
   const [error, setError] = useState<Error | GeolocationPositionError>(null)
 
+  const handleWeatherData = (data: WeatherData) => {
+    cache.storeData('localWeatherData', data)
+    setCurrentWeatherData(data)
+  }
+
+  const handleSetCoords = (coords) => {
+    const cachedLocalWeatherData = cache.getData('localWeatherData')
+
+    if (cachedLocalWeatherData) {
+      if (!currentWeatherData) setCurrentWeatherData(cachedLocalWeatherData)
+    } else {
+      setCoords(coords)
+    }
+  }
+
   useEffect(() => {
     const getWeatherData = (): void => {
-      const success: PositionCallback = ({ coords }: GeolocationPosition): void => setCoords(coords)
+      const success: PositionCallback = ({ coords }: GeolocationPosition): void =>
+        handleSetCoords(coords)
       const error: PositionErrorCallback = (error): void => setError(error)
 
       navigator.geolocation.getCurrentPosition(success, error)
     }
 
     getWeatherData()
+
+    const intervalId: NodeJS.Timeout = setInterval(() => getWeatherData(), REFRESH_TIME_IN_MS)
+
+    return () => clearInterval(intervalId)
   }, [])
 
   useEffect(() => {
     if (coords && !queryCities) {
       const handleCoords = async () => {
         await requestDataByCoords(coords)
-          .then(({ data }) => setCurrentWeatherData(data))
+          .then(({ data }) => handleWeatherData(data))
           .catch((error) => setError(error))
       }
 
@@ -41,7 +62,7 @@ const useWeatherData: (
     if (queryCities) {
       cities.forEach(async (city) => {
         await requestDataByCity(city)
-          .then(({ data }) => setCurrentWeatherData(data))
+          .then(({ data }) => handleWeatherData(data))
           .catch((error) => setError(error))
       })
     }
