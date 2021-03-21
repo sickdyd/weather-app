122f918e920b22bf2d7f26e25b6864dcb5d8dad0
Rob <roberto.reale.ja@gmail.com>
2021-03-21 15:25:49 +0900

    Round up numeric weather data

--- src/api/utils/transformWeatherData.ts
+++ src/api/utils/transformWeatherData.ts
@@ -1,10 +1,10 @@
 const transformWeatherData = (data: OWResponse): WeatherData => ({
   cityName: data.name.toUpperCase(),
   weatherId: data.weather[0].id,
-  temperature: data.main.temp,
-  humidity: data.main.humidity,
+  temperature: Math.round(data.main.temp),
+  humidity: Math.round(data.main.humidity),
   wind: {
-    speed: data.wind.speed,
+    speed: Math.round(data.wind.speed),
     deg: data.wind.deg
   }
 })
