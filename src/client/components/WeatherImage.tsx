b15045a14b376cdc0a2d4b903cafbf07c8263741
Rob <roberto.reale.ja@gmail.com>
2021-04-12 09:42:10 +0900

    Remove icon animation

--- src/client/components/WeatherImage.tsx
+++ src/client/components/WeatherImage.tsx
@@ -5,32 +5,6 @@ const WeatherIcon = styled.i`
   font-size: 11rem;
   margin-top: 1rem;
 
-  @keyframes boing {
-    15%,
-    40%,
-    75%,
-    100% {
-      transform-origin: center center;
-    }
-    15% {
-      transform: scale(1.2, 1.1);
-    }
-    40% {
-      transform: scale(0.95, 0.95);
-    }
-    75% {
-      transform: scale(1.05, 1);
-    }
-    100% {
-      transform: scale(1, 1);
-    }
-  }
-
-  animation-name: boing;
-  animation-direction: forwards;
-  animation-duration: 0.5s;
-  animation-iteration-count: 1;
-
   @media (min-width: 825px) {
     font-size: 15rem;
   }
@@ -41,5 +15,5 @@ const WeatherIcon = styled.i`
 `
 
 export const WeatherImage = ({ weatherId }: { weatherId: number }): JSX.Element => (
-  <WeatherIcon key={Math.random().toString()} className={`wi wi-owm-${weatherId}`} />
+  <WeatherIcon className={`wi wi-owm-${weatherId}`} />
 )
